ba6990c0d55cd2473eb5f1159841183a97257204
Jeffrey Tran <jhtran1234@gmail.com>
2022-12-10 15:49:03 -0500

    Revert "revert language changes"
    This reverts commit b7e547c5dcebf91886e204136fe88f45790f59c0.

--- secretAgent.js
+++ secretAgent.js
@@ -90,15 +90,19 @@ app.get("/addAgent", (request, response) => {
 });
 
 app.post("/processAddAgent", (request, response) => {
+    const firstLetterCap = request.body.language.charAt(0).toUpperCase();
+    const remainingLetters = request.body.language.toLowerCase().slice(1);
+    const language = firstLetterCap + remainingLetters;
+
     const agent = {
         alias: request.body.alias,
-        language: request.body.language
+        language: language
     };
 
     insertAgent(agent);
     response.render("processAddAgent", {
         alias: request.body.alias,
-        language: request.body.language,
+        language: language,
     });
 });
 
